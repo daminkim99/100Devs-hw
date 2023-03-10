3f604eb69d99747a1396b294bcae6686c3c0fd1b
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-10 15:06:28 -0400

    add image to hide upon button, get data from the API server

--- class26hw/todayWeatherApi/js/main.js
+++ class26hw/todayWeatherApi/js/main.js
@@ -1,9 +1,11 @@
-//Example fetch using pokemonapi.co
+//
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  document.querySelector('img').classList.add('hidden')
+  const latitude = document.querySelector('#lat').value
+  const longitude = document.querySelector('#long').value
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,snowfall_sum&forecast_days=1&timezone=auto`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
