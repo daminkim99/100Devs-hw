5e998b1d4344d781edb70ccd46ebd4ea8058c385
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-13 14:54:45 -0300

    modification to the original pokemon via prof

--- class26hw/api3/js/main.js
+++ class26hw/api3/js/main.js
@@ -2,16 +2,45 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const poke1 = document.querySelector('#poke1').value
+  const poke2 = document.querySelector('#poke2').value
+  const url = 'https://pokeapi.co/api/v2/pokemon/'+poke1
+  const url2 = 'https://pokeapi.co/api/v2/pokemon/'+poke2
+  let pokeStore = []
+  let pokeImg = []
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        pokeStore.push(data.types[0].type.name)
+        pokeImg.push(data.sprites.front_shiny)
+        
+        fetch(url2)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+
+          pokeStore.push(data.types[0].type.name)
+          pokeImg.push(data.sprites.front_shiny)
+          console.log(pokeStore)
+         
+          document.querySelector('#pokeImg1').src = pokeImg[0]
+          document.querySelector('#pokeImg2').src = pokeImg[1]
+          document.querySelector('#type1').innerText = `${poke1} type : ${pokeStore[0]}`
+          document.querySelector('#type2').innerText = `${poke2} type : ${pokeStore[1]}`
+        
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+
+
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-}
 
+
+
+      
+}
